ffb6bac0025f8c5d08632aacd23beac8ba63fd17
Amy <aschultz@invoca.com>
2015-10-10 19:53:29 -0700

    HTML parse functionality working, switched typed variables for Constants near top

--- parse.rb
+++ parse.rb
@@ -1,7 +1,14 @@
 require 'open-uri'
 require 'nokogiri'
 
-doc = Nokogiri::HTML(open("http://www.reddit.com/r/gifs/.xml"))
-doc.xpath('//item/title').each do |node|
-  puts node.text  
+# Parse Token: <div class="usertext-body may-blank-within md-container "><div class="md">
+
+COMMENT_PARSE_KEY = '//div[@class="usertext-body may-blank-within md-container "]/div[@class="md"]'
+URL_TO_PARSE = "https://www.reddit.com/r/gifs/comments/3o956q/everest_the_kitten_way/"
+URL_TO_ITERATE = "http://www.reddit.com/r/gifs/"
+
+doc = Nokogiri::HTML(open(URL_TO_PARSE))
+doc.xpath(COMMENT_PARSE_KEY).each do |node|
+  puts node.text
+  puts
 end
