93970d0502c730784a7d30bcd5f011accf593b00
Amy <aschultz@invoca.com>
2015-10-13 21:09:34 -0700

    Forgot parse + replace

--- parse.rb
+++ parse.rb
@@ -1,79 +1,99 @@
-
-
 class Parse
 
 
 URL_PARSE_KEY = '//a[@class="title may-blank "]/@href'
 COMMENT_PARSE_KEY = '//div[@class="usertext-body may-blank-within md-container "]/div[@class="md"]'
 TITLE_PARSE_KEY = '//a[@class="title may-blank "]'
-URL_TO_PARSE = "https://www.reddit.com/r/gifs/comments/3o956q/everest_the_kitten_way/"
+URL_TO_PARSE = "https://www.reddit.com/r/gifs/comments/3okq6n/in_a_rare_encounter_divers_come_facetoface_with/"
 URL_TO_CRAWL = "https://www.reddit.com/r/gifs/"
-OFFLINE_PARSE_URL = "./kittens.html"
+OFFLINE_PARSE_URL = "./molamola.html"
 OFFLINE_CRAWL_URL = "./gifshead.html"
 
 
-
+# # # # # # # # # # # # # # # #
 def initialize
   @links = []
   @titles = []
   @comments = []
 
-  puts @@clock.now.strftime(CLOCK_FORMAT ) + "Parser standing by."
+  puts Time.now.strftime(CLOCK_FORMAT) << "Parser standing by."
 end
 
+attr_accessor :links
+attr_accessor :titles
+attr_accessor :comments
 
 # Crawls through URL_TO_CRAWL in order to find threads for parse_for_comments
 # URL_PARSE_KEY dictates the current method of searching for links.
 # # # # # # # # # # # # # # # #
-def parse_for_links(online: true)
-  puts @@clock.now.strftime(CLOCK_FORMAT) + "Parser: I am now parsing for links."
+def parse_for_links(online: true, verbose: false)
+  puts Time.now.strftime(CLOCK_FORMAT) << "Parser: I am now parsing for links."
 
   if online
     sub = Nokogiri::HTML(open(URL_TO_CRAWL))
     sub.xpath(URL_PARSE_KEY).each do |node|
-      puts @@clock.now.strftime(CLOCK_FORMAT) + "Parser: Found one! #{node.text}"
       @links << node.text
+      if verbose
+        if node.text.size > 35
+          puts Time.now.strftime(CLOCK_FORMAT) << "Parser: Found one! ... it's a big one!"
+        else
+          puts Time.now.strftime(CLOCK_FORMAT) << "Parser: Found one! #{node.text}"
+        end
+      end
     end
   else
-    sub = Nokogiri::HTML(open(OFFLINE_PARSE_URL))
+    sub = Nokogiri::HTML(open(OFFLINE_CRAWL_URL))
     sub.xpath(URL_PARSE_KEY).each do |node|
-      puts @@clock.now.strftime(CLOCK_FORMAT) + "Parser: Found one! #{node.text}"
       @links << node.text
+      if verbose
+        if node.text.size > 35
+          puts Time.now.strftime(CLOCK_FORMAT) << "Parser: Found one! ... it's a big one!"
+        else
+          puts Time.now.strftime(CLOCK_FORMAT) << "Parser: Found one! #{node.text}"
+        end
+      end
     end
   end
 end
 
 
 # Crawls through URL_TO_CRAWL in order to find titles to later manipulate
-# for hilarious ends. TODO Write Manip.
+# for hilarious ends.
 # COMMENT_PARSE_KEY dictates the current method of searching for links.
 # # # # # # # # # # # # # # # #
-def parse_for_titles(online: true)
-  puts @@clock.now.strftime(CLOCK_FORMAT) + "Parser: I am now parsing for titles."
+def parse_for_titles(online: true, verbose: false)
+  puts Time.now.strftime(CLOCK_FORMAT) << "Parser: I am now parsing for titles."
 
   if online
     titles = Nokogiri::HTML(open(URL_TO_CRAWL))
     titles.xpath(COMMENT_PARSE_KEY).each do |node|
-      puts @@clock.now.strftime(CLOCK_FORMAT) + "Parser: Found one!"
+      if verbose
+        puts Time.now.strftime(CLOCK_FORMAT) << "Parser: Found one!"
+      end
       @titles << node.text
     end
   else
     titles = Nokogiri::HTML(open(OFFLINE_CRAWL_URL))
     titles.xpath(COMMENT_PARSE_KEY).each do |node|
-      puts @@clock.now.strftime(CLOCK_FORMAT) + "Parser: Found one!"
+      if verbose
+        puts Time.now.strftime(CLOCK_FORMAT) << "Parser: Found one!"
+      end
       @titles << node.text
     end
   end
 end
 
 # Parses through URL_TO_PARSE in order to find comments to later manipulate
-# for hilarious ends. TODO Write Manip.
+# for hilarious ends.
 # # # # # # # # # # # # # # # #
 def parse_for_comments(url, online: true)
-  if online
-    thread = Nokogiri::HTML(open(url))
-    thread.xpath(COMMENT_PARSE_KEY).each do |node|
-      @comments << node.text
+  puts Time.now.strftime(CLOCK_FORMAT) << "Parser: I am now parsing for comments."
+
+                                                          # TODO CONT.
+  if online                                               # 1. Gather Usernames.
+    thread = Nokogiri::HTML(open(url))                    # 2. Build Comment per Username.
+    thread.xpath(COMMENT_PARSE_KEY).each do |node|        # 3. Gather text-bodies.
+      @comments << node.text                              # 4. Add text-bodies to Comments.
     end
   else
     thread = Nokogiri::HTML(open(url))
@@ -83,7 +103,7 @@ def parse_for_comments(url, online: true)
   end
 end
 
-# Checks @links any contents at all.
+# Checks @links for any content at all.
 # If empty, save some time and don't try and print anything.
 # # # # # # # # # # # # # # # #
 def empty_links?
@@ -93,21 +113,13 @@ def empty_links?
 end
 
 
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Getters / Setters below.
 
-def get_links
-  @links
-end
 
-def get_titles
-  @titles
-end
 
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Print Methods Below.
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@ Print Methods Below.
 
 
 # Prints all links gathered by parse_for_links
-#
 # # # # # # # # # # # # # # # #
 def print_all_links
   @links.each do |link|
@@ -117,7 +129,6 @@ end
 
 
 # Prints all links gathered by parse_for_comments
-#
 # # # # # # # # # # # # # # # #
 def print_all_comments
   @comments.each do |comment|
@@ -126,7 +137,6 @@ def print_all_comments
 end
 
 # Prints all links gathered by parse_for_comments
-#
 # # # # # # # # # # # # # # # #
 def print_all_titles
   @titles.each do |title|
@@ -134,5 +144,14 @@ def print_all_titles
   end
 end
 
+
+end
+
+class Comment
+
+  def initialize(user, text)
+    @user = user
+    @text = text
+  end
 
 end
--- replace.rb
+++
@@ -1,7 +0,0 @@
-class Replace
-
-  def initialize
-    puts @@clock.now.strftime(CLOCK_FORMAT) + "Replacer standing by."
-  end
-
-end
